5775043a1316d5d1cfc779c1b27adc2b2cc08590
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2022-01-14 17:46:53 -0700

    Update [slug].js

--- pages/simplestats/[slug].js
+++ pages/simplestats/[slug].js
@@ -9,7 +9,7 @@ export default function SimpleStats({ mdxSource, frontMatter }) {
         components: MDXComponents
     })
 
-    return <SimpleStatsLayout className= "animate__animated animate__fadeIn" frontMatter={frontMatter}>{content}</SimpleStatsLayout>
+    return <div className= "animate__animated animate__fadeIn animate__delay-1s"><SimpleStatsLayout frontMatter={frontMatter}>{content}</SimpleStatsLayout></div>
 }
 
 export async function getStaticPaths() {
