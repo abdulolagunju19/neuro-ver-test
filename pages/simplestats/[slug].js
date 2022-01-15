baff19280237d9544e1c9cf52ab9536df1328883
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2022-01-14 17:36:00 -0700

    Update [slug].js

--- pages/simplestats/[slug].js
+++ pages/simplestats/[slug].js
@@ -2,13 +2,14 @@ import hydrate from 'next-mdx-remote/hydrate'
 import { getFiles, getFileBySlug } from '../../lib/mdx'
 import SimpleStatsLayout from '../../layouts/simplestats'
 import MDXComponents from '../../components/MDXComponents'
+import "animate.css"
 
 export default function SimpleStats({ mdxSource, frontMatter }) {
     const content = hydrate(mdxSource, {
         components: MDXComponents
     })
 
-    return <SimpleStatsLayout frontMatter={frontMatter}>{content}</SimpleStatsLayout>
+    return <SimpleStatsLayout className= "animate__animated animate__fadeIn" frontMatter={frontMatter}>{content}</SimpleStatsLayout>
 }
 
 export async function getStaticPaths() {
