94eeeb1cb9c3635e4a9a0a0108a58e4ccbf18e01
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2022-01-14 17:35:26 -0700

    Update [slug].js

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -2,13 +2,14 @@ import hydrate from 'next-mdx-remote/hydrate'
 import { getFiles, getFileBySlug } from '../../lib/mdx'
 import BlogLayout from '../../layouts/blog'
 import MDXComponents from '../../components/MDXComponents'
+import "animate.css"
 
 export default function Blog({ mdxSource, frontMatter }) {
     const content = hydrate(mdxSource, {
         components: MDXComponents
     })
 
-    return <BlogLayout frontMatter={frontMatter}>{content}</BlogLayout>
+    return <BlogLayout className= "animate__animated animate__fadeIn" frontMatter={frontMatter}>{content}</BlogLayout>
 }
 
 export async function getStaticPaths() {
