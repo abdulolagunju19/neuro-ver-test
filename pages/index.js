7106344117aa03aed5709873b70350724f5fa630
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2022-01-14 18:37:40 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,7 @@ export default function Index() {
   const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
   const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
   const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
-  const isMobile = useMediaQuery({ maxWidth: 767 })
+  const isMobile = useMediaQuery({ maxWidth: 640 })
   
   const pulse = keyframes`
     0% {
